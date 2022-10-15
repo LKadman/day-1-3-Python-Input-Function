7b9e7213072f0cee1bf259f3a486cc1072dfd8c7
Lot Kaduma <42066033+LKadman@users.noreply.github.com>
2022-10-15 12:28:25 +0000

    Description

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+# The Input Function
+
+# looks identical to the print function
+
+# print("what is your name?") outputs the exact text in the function and the code execution ends immediately
+
+# The input () function gives our code some data to work with. 
+# When a user is asked a question, the program uses the data provided by the user 
+
+# input("what is your name?") outputs the prompt and the cursor stays in the same line for the user to input the data
+# when the user presses enter, the code execution ends, the data is parsed back into the code and replaces the original input code
+
+#  instead of adding text to be printed, a prompt for the user is added in the perentheses to give a hint of what kind of data is needed
+#      input('a prompt for the user')
+# running the code prints out the prompt followed by a cursor, ready for the user to type the data
+
+# Run input("What is your name?")
+
+# we enter the data eg 'Angela', the data replaces the code
+# 'Angela' replaces 'input("What is your name?")'
+
+# to print "Hello Angela"
+
+print("Hello " + input("What is your name?"))
+
+
+
+
+
+
+
 #Write your code below this line 👇
 
 
